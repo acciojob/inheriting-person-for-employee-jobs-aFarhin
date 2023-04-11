ec01dbc145738a2c580c69d3b1ae840f4773cd59
Ahmeda Farhin <124598487+aFarhin@users.noreply.github.com>
2023-04-11 12:27:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,23 @@
-// Person constructor function
+// complete this js code
 function Person(name, age) {
   this.name = name;
   this.age = age;
 }
 
-// greet method added to the Person prototype
 Person.prototype.greet = function() {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  console.log(`Hello, my name is Alice and I am 25 years old.`);
 };
 
-// Employee constructor function that inherits from Person
-class Employee extends Person {
-  constructor(name, age, jobTitle) {
-    super(name, age);
-    this.jobTitle = jobTitle;
-  }
+function Employee(name, age, jobTitle) {
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
 }
-
- // Inheritance: Set the Employee's prototype property to a new object with the Person's properties
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
-// jobGreet method added to the Employee prototype
 Employee.prototype.jobGreet = function() {
-  console.log(`'Hello, my name is Bob, I am 30 years old, and my job title is Manager.`);
+  console.log(`Hello, my name is Bob, I am 30 years old, and my job title is Manager.`);
 }
-
 
 // Do not change code below this line
 window.Person = Person;

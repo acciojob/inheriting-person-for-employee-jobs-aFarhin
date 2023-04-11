dccc9c2044299ab27b87245126153aa15b6ef23a
Ahmeda Farhin <124598487+aFarhin@users.noreply.github.com>
2023-04-11 12:03:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -24,7 +24,8 @@ Employee.prototype.constructor = Employee;
 // jobGreet method added to the Employee prototype
 Employee.prototype.jobGreet = function() {
   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-};
+}
+
 
 // Do not change code below this line
 window.Person = Person;
